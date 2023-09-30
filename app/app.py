251a08f93c7ab7981a87bf869dabf1044b506927
NARENDIR <97464705+NARENDIR@users.noreply.github.com>
2023-09-30 14:44:51 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hi everyone to devops!'
+    return 'Hi everyone to devops AWS!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
